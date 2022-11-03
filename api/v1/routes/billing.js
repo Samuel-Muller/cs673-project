681171c0342e6a0d0648ac8311adcfb27c00626f
Lucas Bastos <lucas@lucasbastos.com>
2022-11-03 12:18:05 -0400

    Rework routing and add swaggerdocs

---
+++ api/v1/routes/billing.js
@@ -0,0 +1,208 @@
+const express = require('express')
+const router = express.Router()
+
+/**
+* @openapi
+* /payments:
+*   get:
+*     tags:
+*       - Payments
+*     produces:
+*       - application/json
+*     description: Get all payments
+*     responses:
+*       200:
+*         description: Returns payments
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to access payments
+*       404:
+*         description: Payments not found
+*       500:
+*         description: Internal server error
+*   post:
+*     tags:
+*       - Payments
+*     description: Create new payment
+*     responses:
+*       200:
+*         description: Payment created
+*       400:
+*         description: Invalid payment details
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to create payment
+*       500:
+*         description: Internal server error
+*/
+router.route('/payments')
+    .get((req, res) => {
+        res.send('TODO: get all payments')
+    })
+    .post((req, res) => {
+        res.send('TODO: create payment')
+    })
+
+/**
+* @openapi
+* /payments/{payment_id}:
+*   get:
+*     tags:
+*       - Payments
+*     produces:
+*       - application/json
+*     description: Get payment details
+*     responses:
+*       200:
+*         description: Returns payment details
+*       400:
+*         description: Invalid payment ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to access payment details
+*       404:
+*         description: Payment details not found
+*       500:
+*         description: Internal server error
+*   put:
+*     tags:
+*       - Payments
+*     description: Update payment details
+*     responses:
+*       200:
+*         description: Payment updated
+*       400:
+*         description: Invalid payment ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to update payment details
+*       404:
+*         description: Payment details not found
+*       500:
+*         description: Internal server error
+*/
+router.route('/payments/:payment_id')
+    .get((req, res) => {
+        res.send('TODO: get payment info')
+    })
+    .put((req, res) => {
+        res.send('TODO: update payment')
+    })
+
+/**
+* @openapi
+* /reports:
+*   get:
+*     tags:
+*       - Reports
+*     produces:
+*       - application/json
+*     description: Get all reports
+*     responses:
+*       200:
+*         description: Returns reports
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to access reports
+*       404:
+*         description: Reports not found
+*       500:
+*         description: Internal server error
+*   post:
+*     tags:
+*       - Reports
+*     description: Create new report
+*     responses:
+*       200:
+*         description: Report created
+*       400:
+*         description: Invalid report details
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to create report
+*       500:
+*         description: Internal server error
+*/
+router.route('/reports')
+    .get((req, res) => {
+        res.send('TODO: get all reports')
+    })
+    .post((req, res) => {
+        res.send('TODO: create report')
+    })
+
+/**
+* @openapi
+* /reports/{report_id}:
+*   get:
+*     tags:
+*       - Reports
+*     produces:
+*       - application/json
+*     description: Get report by ID
+*     responses:
+*       200:
+*         description: Returns report
+*       400:
+*         description: Invalid report ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to access report
+*       404:
+*         description: Report not found
+*       500:
+*         description: Internal server error
+*   put:
+*     tags:
+*       - Reports
+*     description: Update report by ID
+*     responses:
+*       200:
+*         description: Report updated
+*       400:
+*         description: Invalid report ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to update report
+*       404:
+*         description: Report not found
+*       500:
+*         description: Internal server error
+*   delete:
+*     tags:
+*       - Reports
+*     description: Delete report by ID
+*     responses:
+*       200:
+*         description: Report deleted
+*       400:
+*         description: Invalid report ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to delete report
+*       404:
+*         description: Report not found
+*       500:
+*         description: Internal server error
+*/
+router.route('/reports/:report_id')
+    .get((req, res) => {
+        res.send('TODO: get report info')
+    })
+    .put((req, res) => {
+        res.send('TODO: update report')
+    })
+    .delete((req, res) => {
+        res.send('TODO: delete report')
+    })
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,36 +1,37 @@
 const express = require('express')
+const billing = require('./api/v1/routes/billing')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJSDoc = require('swagger-jsdoc');
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Billing and Revenue Management API',
+            version: '1.0.0',
+        },
+        basePath: '/api/v1'
+    },
+    apis: ['./api/v1/routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
 const app = express()
 const port = 3000
 
-//TODO: document each API endpoint
-app.get('/billing/payment', (req, res) => {
-    res.send('TODO: get payment info')
+// Import the billing router
+app.use('/api/v1/billing', billing)
+
+// Log all requests and responses to the console
+app.use((req, res, next) => {
+    const date = new Date()
+    console.log(date.toISOString() + ' ' + req.ip + ' ' + req.method + ' ' + req.originalUrl)
+    next()
 })
 
-app.post('/billing/payment', (req, res) => {
-    res.send('TODO: create payment')
-})
 
-app.put('/billing/payment', (req, res) => {
-    res.send('TODO: update payment')
-})
+// Serve the Swagger UI
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
-app.get('/billing/report', (req, res) => {
-    res.send('TODO: get report info')
-})
-
-app.post('/billing/report', (req, res) => {
-    res.send('TODO: create report')
-})
-
-app.put('/billing/report', (req, res) => {
-    res.send('TODO: update report')
-})
-
-app.delete('/billing/report', (req, res) => {
-    res.send('TODO: delete report')
-})
-
+// Start the server
 app.listen(port, () => {
     console.log(`API listening on port ${port}`)
 })
