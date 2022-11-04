abe82eb4c8475f545ee4cec157c62275dcb6b44d
Lucas Bastos <lucas@lucasbastos.com>
2022-11-03 21:23:24 -0400

    Add DELETE to payments route

--- api/v1/routes/billing.js
+++ api/v1/routes/billing.js
@@ -9,7 +9,7 @@ const router = express.Router()
 *       - Payments
 *     produces:
 *       - application/json
-*     description: Get all payments
+*     description: Get all payments localhost:3000/billing/payments
 *     responses:
 *       200:
 *         description: Returns payments
@@ -84,6 +84,23 @@ router.route('/payments')
 *         description: Payment details not found
 *       500:
 *         description: Internal server error
+*   delete:
+*     tags:
+*       - Payments
+*     description: Delete payment by ID
+*     responses:
+*       200:
+*         description: Payment deleted
+*       400:
+*         description: Invalid payment ID
+*       401:
+*         description: Client not authorized
+*       403:
+*         description: User is not authorized to delete payment
+*       404:
+*         description: Payment not found
+*       500:
+*         description: Internal server error
 */
 router.route('/payments/:payment_id')
     .get((req, res) => {
@@ -91,6 +108,9 @@ router.route('/payments/:payment_id')
     })
     .put((req, res) => {
         res.send('TODO: update payment')
+    })
+    .deklete((req, res) => {
+        res.send('TODO: delete payment')
     })
 
 /**
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express')
-const billing = require('./api/v1/routes/billing')
 const swaggerUI = require('swagger-ui-express')
 const swaggerJSDoc = require('swagger-jsdoc');
+const billing = require('./api/v1/routes/billing')
 const options = {
+    failOnErrors: true,
     definition: {
         openapi: '3.0.0',
         info: {
             title: 'Billing and Revenue Management API',
+            description: 'API for managing billing and revenue',
             version: '1.0.0',
         },
-        basePath: '/api/v1'
+        servers: [{ url: '/billing' }],
     },
     apis: ['./api/v1/routes/*.js'],
 };
 const swaggerSpec = swaggerJSDoc(options);
 const app = express()
 const port = 3000
-
-// Import the billing router
-app.use('/api/v1/billing', billing)
 
 // Log all requests and responses to the console
 app.use((req, res, next) => {
@@ -27,9 +26,11 @@ app.use((req, res, next) => {
     next()
 })
 
+// Import the billing router
+app.use('/billing', billing)
 
 // Serve the Swagger UI
-app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 // Start the server
 app.listen(port, () => {
